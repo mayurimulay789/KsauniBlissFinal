d6a522dd5907f6c14e958db4705f69304a30660d
test982298 <test982298@gmail.com>
2025-09-12 10:46:08 +0530

    Optimize Docker build: reduce memory usage and fix build conflicts

---
+++ client/src/store/api/index.js
@@ -0,0 +1,13 @@
+// API exports for easier importing
+export { default as adminAPI } from './adminAPI';
+export { default as authAPI } from './authAPI';
+export { default as cartAPI } from './cartAPI';
+export { default as wishlistAPI } from './wishlistAPI';
+export { default as productAPI } from './ProductAPI';
+export { default as reviewAPI } from './reviewAPI';
+export { default as orderAPI } from './orderAPI';
+export { default as returnAPI } from './returnAPI';
+export { default as couponAPI } from './couponAPI';
+export { default as bannerAPI } from './bannerAPI';
+export { default as categoryAPI } from './categoryAPI';
+export { default as digitalMarketerAPI } from './digitalMarketerAPI';
