aa22271b585e22769d1e27977a8e1a31852d25ed
Mayuri Mulay <mayurimulay789@gmail.com>
2025-12-18 10:36:02 +0530

    added

--- client/src/components/FlatDiscount.jsx
+++ client/src/components/FlatDiscount.jsx
@@ -1,16 +1,16 @@
 import { useNavigate } from "react-router-dom";
-import discount from "../../public/discount (1).jpg";
+import discount from "../../public/Trending.png";
 const FlatDiscount = () => {
   const navigate = useNavigate();
   const handleClick = () => {
     navigate("/products"); // Navigates to all products page
   };
   return (
-    <div className="w-full cursor-pointer px-2" onClick={handleClick}>
+    <div className="w-full  cursor-pointer px-2 mt-1 mb-1" onClick={handleClick}>
       <img
         src={discount} // Image from public folder
         alt="Discount Offer"
-        className="w-full h-auto object-cover"
+        className="w-full h-auto object-cover rounded-xl"
       />
     </div>
   );
